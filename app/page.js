d41170cd96e657037ad457553a13bf1681109576
hedamir <hed.amir@gmail.com>
2026-03-19 13:25:48 -0700

    page.js

--- app/page.js
+++ app/page.js
@@ -359,6 +359,7 @@ export default function App() {
           vibe: setVibe,
           userId: user?.id,
           refreshToken,
+          token,
         }),
       })
       const data = await res.json()
